d4703f6e0ab5dd27164e0429dc99a6982ff25e4b
wes-johnson <wes0johnson@gmail.com>
2016-05-13 09:56:03 -0700

    Added third party license files and copyright statements

--- stand_alone_examples/python_example/example.py
+++ stand_alone_examples/python_example/example.py
@@ -1,4 +1,15 @@
 #!/usr/local/bin/python
+######################################################################
+# Copyright (c) 2012, 2016 Cirrus Link Solutions
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Cirrus Link Solutions
+######################################################################
 
 import paho.mqtt.client as mqtt
 import kurapayload_pb2
